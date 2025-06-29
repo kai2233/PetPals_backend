039b01f51fa02005d3b378d08a974c5d760aca7b
Kaifeng Yu <1536771545kai@gmail.com>
2025-06-28 21:33:35 -0400

    feat: add refreshAccessToken endpoint

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,6 +4,7 @@ const usersController = require("../controllers/usersController");
 
 router.post('/signup', usersController.signup)
 router.post('/login', usersController.login)
+router.put('/refreshAccessToken', usersController.refreshAccessToken)
 
 
 
